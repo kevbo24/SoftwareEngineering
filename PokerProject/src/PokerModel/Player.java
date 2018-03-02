ebad060a527994b9e68d201047cf9b871fe4a190
kevbo24 <36130935+kevbo24@users.noreply.github.com>
2018-03-02 15:21:03 +0100

    Add files via upload

---
+++ PokerProject/src/PokerModel/Player.java
@@ -0,0 +1,20 @@
+package PokerModel;
+
+public class Player {
+
+    public String playerName;
+	
+	
+	//constructor
+    public Player(String playerName) {
+		this.playerName = playerName;
+		}
+    
+    
+    public String getPlayerName() {
+    	return playerName;
+    }
+    
+    
+
+}
